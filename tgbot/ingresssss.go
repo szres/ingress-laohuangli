deceef4793941b56a265421b58ae3b93824a6776
xianii <xianyi.xia@outlook.com>
2024-08-22 15:17:38 +0800

    update ingress event

--- tgbot/ingresssss.go
+++ tgbot/ingresssss.go
@@ -22,20 +22,47 @@ func ingressStr() string {
 	now := time.Now()
 	if now.Weekday() == time.Saturday && now.Day() <= 7 {
 		// IFS day
-		if PP(17) {
-			return "参加IFS"
+		if PP(27) {
+			results := []string{
+				"参加IFS",
+				"女装参加IFS",
+				"带猫猫参加IFS",
+				"带狗狗参加IFS",
+				"不带手机参加IFS",
+				"在IFS现场转生",
+			}
+			randInt, _ := rand.Int(rand.Reader, big.NewInt(int64(len(results))))
+			return results[randInt.Int64()]
 		}
 	}
 	if now.Weekday() == time.Tuesday {
 		// Double AP day
-		if PP(13) {
-			return "刷AP"
+		if PP(21) {
+			results := []string{
+				"刷AP",
+				"女装刷AP",
+				"嗑APEX刷AP",
+				"请假刷AP",
+				"翘班刷AP",
+				"不带手机刷AP",
+			}
+			randInt, _ := rand.Int(rand.Reader, big.NewInt(int64(len(results))))
+			return results[randInt.Int64()]
 		}
 	}
 	if now.Weekday() == time.Sunday && now.Day() > 7 && now.Day() <= 14 {
 		// ISS day
-		if PP(11) {
-			return "出门做一排任务"
+		if PP(17) {
+			results := []string{
+				"出门做一排任务",
+				"出门做两排任务",
+				"出门做三排任务",
+				"出门做四排任务",
+				"女装出门做一排任务",
+				"不带手机做一排任务",
+			}
+			randInt, _ := rand.Int(rand.Reader, big.NewInt(int64(len(results))))
+			return results[randInt.Int64()]
 		}
 	}
 	return ""
