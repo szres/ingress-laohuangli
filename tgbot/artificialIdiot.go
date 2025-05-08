21241ecd94da7001bbf69f85cd021eb946a2c339
xianii <xianyi.xia@outlook.com>
2025-05-09 02:18:07 +0800

    opt AI queue

--- tgbot/artificialIdiot.go
+++ tgbot/artificialIdiot.go
@@ -27,7 +27,7 @@ var AISamples []string = []string{
 	"胡萝卜玉米猪骨汤",
 }
 
-var promptDefault = "你是一个算命机器人，会随机给出一个今天的幸运物或者行为词条，词条范围包含但不限于与今天日期相关的节日活动、Ingress游戏中的行为、衣服穿搭、发型发色、交通工具、饮食搭配、经典网络迷因等等各种有趣的东西；其中，Ingress中的名词均使用英文。词条必须简短不含逗号，但是需要搞笑有趣、幽默讽刺。当今天是节日时，生成词条尽量与节日相关，生成词条均以大括号{}括住，请参考后面的生成词条示例，再生成10条词条。\n"
+var promptDefault = "你是一个算命机器人，会随机给出一个今天的幸运物或者行为词条，词条范围包含但不限于与今天日期相关的节日活动、Ingress游戏中的行为、衣服穿搭、发型发色、交通工具、饮食搭配、经典网络迷因等等各种有趣的东西；其中，Ingress中的名词均使用英文。词条必须简短不含逗号，但是需要搞笑有趣、幽默讽刺。当今天是节日时，生成词条尽量与节日相关，生成词条均以大括号{}括住，请参考后面的生成词条示例，再生成13条词条。\n"
 var promptEnd = "请仅回答生成的词条，每个词条一行。"
 
 func todayChineseDate() string {
@@ -38,11 +38,18 @@ func todayChineseDate() string {
 type AIInstance struct {
 	Name   string
 	Init   func(*AIInstance)
-	Update func(*AIInstance)
-	Pool   []string
+	Update func(*AIInstance) error
 }
 
+var AIContentPool []string
 var AIs []*AIInstance
+
+func AIContentPush(s string) {
+	AIContentPool = append(AIContentPool, s)
+	for len(AIContentPool) > 15 {
+		AIContentPool = AIContentPool[1:]
+	}
+}
 
 func initAIs() {
 	AIs = make([]*AIInstance, 0)
@@ -52,13 +59,50 @@ func initAIs() {
 		Update: getContentOpenAI,
 	})
 	AIs = append(AIs, &AIInstance{
-		Name:   "Gemini2.0flash",
+		Name:   "Gemini2.5flash",
 		Init:   initGemini,
 		Update: getContentGemini,
 	})
 	for _, ai := range AIs {
 		ai.Init(ai)
 	}
+
+	go func() {
+		ticker := time.NewTicker(30 * time.Second)
+		for range ticker.C {
+			for _, ai := range AIs {
+				if len(AIContentPool) < 5 {
+					ai.Update(ai)
+				}
+			}
+		}
+	}()
+
+	go func() {
+		for {
+			var updated bool
+			if len(AIContentPool) < 5 {
+				for _, ai := range shuffle(AIs) {
+					if err := ai.Update(ai); err == nil {
+						updated = true
+						break
+					}
+				}
+				if !updated {
+					fmt.Println("All AIs failed to update")
+				}
+			}
+			<-time.After(30 * time.Second)
+		}
+	}()
+}
+
+func shuffle(arr []*AIInstance) []*AIInstance {
+	rand.Seed(uint64(time.Now().UnixNano()))
+	rand.Shuffle(len(arr), func(i, j int) {
+		arr[i], arr[j] = arr[j], arr[i]
+	})
+	return arr
 }
 
 func initGemini(self *AIInstance) {
@@ -67,20 +111,11 @@ func initGemini(self *AIInstance) {
 		fmt.Println("Gemini api key is empty")
 		return
 	}
-	self.Pool = make([]string, 0)
 
 	getContentGemini(self)
-	go func() {
-		ticker := time.NewTicker(30 * time.Second)
-		for range ticker.C {
-			if len(self.Pool) < 10 {
-				getContentGemini(self)
-			}
-		}
-	}()
 }
 
-func getContentGemini(self *AIInstance) {
+func getContentGemini(self *AIInstance) (err error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
 	client, err := gemini.NewClient(ctx, option.WithAPIKey(os.Getenv("GEMINI_API_KEY")))
@@ -90,7 +125,7 @@ func getContentGemini(self *AIInstance) {
 	}
 	defer client.Close()
 
-	model := client.GenerativeModel("gemini-2.0-flash-exp")
+	model := client.GenerativeModel("gemini-2.5-flash-preview-04-17")
 	resp, err := model.GenerateContent(ctx, gemini.Text(getPrompt()))
 	if err != nil {
 		fmt.Println("Error", err)
@@ -104,17 +139,14 @@ func getContentGemini(self *AIInstance) {
 				for _, v := range lines {
 					match := re.FindStringSubmatch(v)
 					if len(match) > 0 {
-						self.Pool = append(self.Pool, match[1])
+						AIContentPush(match[1])
 						fmt.Println(self.Name, "AI result add:", match[1])
-					}
-					for len(self.Pool) >= 15 {
-						self.Pool = self.Pool[1:]
 					}
 				}
 			}
 		}
 	}
-
+	return err
 }
 
 func initOpenAI(self *AIInstance) {
@@ -123,39 +155,22 @@ func initOpenAI(self *AIInstance) {
 		fmt.Println("OpenAI api key is empty")
 		return
 	}
-	self.Pool = make([]string, 0)
 
 	gptClient = openai.NewClientWithConfig(openai.DefaultConfig(apiKey))
 
 	getContentOpenAI(self)
-	go func() {
-		ticker := time.NewTicker(30 * time.Second)
-		for range ticker.C {
-			if len(self.Pool) < 10 {
-				getContentOpenAI(self)
-			}
-		}
-	}()
 }
 
 func AIContentValid() bool {
-	sum := 0
-	for _, v := range AIs {
-		sum += len(v.Pool)
-	}
-	return sum > 0
+	return len(AIContentPool) > 0
 }
 
-func AIContentPop() (result string, name string) {
-	aiIdx := rand.Intn(len(AIs))
-	if len(AIs[aiIdx].Pool) == 0 {
-		return "", ""
-	}
-	resultIdx := rand.Intn(len(AIs[aiIdx].Pool))
-	result = AIs[aiIdx].Pool[resultIdx]
-	AIs[aiIdx].Pool = append(AIs[aiIdx].Pool[:resultIdx], AIs[aiIdx].Pool[resultIdx+1:]...)
-	fmt.Println(AIs[aiIdx].Name, "len:", len(AIs[aiIdx].Pool), "result:", result)
-	return result, AIs[aiIdx].Name
+func AIContentPop() (result string) {
+	resultIdx := rand.Intn(len(AIContentPool))
+	result = AIContentPool[resultIdx]
+	AIContentPool = append(AIContentPool[:resultIdx], AIContentPool[resultIdx+1:]...)
+	fmt.Println("len:", len(AIContentPool), "result:", result)
+	return result
 }
 
 func AISampleApped(s string) {
@@ -165,7 +180,7 @@ func AISampleApped(s string) {
 	}
 }
 
-func getContentOpenAI(self *AIInstance) {
+func getContentOpenAI(self *AIInstance) (err error) {
 	prompt := make([]openai.ChatCompletionMessage, 0)
 	prompt = append(prompt, openai.ChatCompletionMessage{
 		Role:    openai.ChatMessageRoleSystem,
@@ -185,20 +200,19 @@ func getContentOpenAI(self *AIInstance) {
 	)
 	if err != nil {
 		fmt.Println("Error", err)
-	} else {
-		lines := strings.Split(resp.Choices[0].Message.Content, "\n")
-		re := regexp.MustCompile(`\{宜(.+?)\}`)
-		for _, v := range lines {
-			match := re.FindStringSubmatch(v)
-			if len(match) > 0 {
-				self.Pool = append(self.Pool, match[1])
-				fmt.Println(self.Name, "AI result add:", match[1])
-			}
-			for len(self.Pool) >= 15 {
-				self.Pool = self.Pool[1:]
-			}
+		return
+	}
+
+	lines := strings.Split(resp.Choices[0].Message.Content, "\n")
+	re := regexp.MustCompile(`\{宜(.+?)\}`)
+	for _, v := range lines {
+		match := re.FindStringSubmatch(v)
+		if len(match) > 0 {
+			AIContentPush(match[1])
+			fmt.Println(self.Name, "AI result add:", match[1])
 		}
 	}
+	return err
 }
 
 func getPrompt() string {
