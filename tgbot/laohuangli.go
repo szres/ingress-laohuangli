1bfc2387ce0ca0feee2c4602bd4395e5ed008e8c
xianii <xianyi.xia@outlook.com>
2025-09-24 12:18:51 +0800

    fix special day

--- tgbot/laohuangli.go
+++ tgbot/laohuangli.go
@@ -336,6 +336,9 @@ func (lhl *laohuangli) randomToday(id int64, name string) string {
 			str = ingressStr()
 		}
 		if str == "" {
+			str = specialDay()
+		}
+		if str == "" {
 			randInt, _ := rand.Int(rand.Reader, big.NewInt(int64(25600)))
 			if AIContentValid() && randInt.Cmp(big.NewInt(15360)) >= 0 {
 				AIContent := AIContentPop()
